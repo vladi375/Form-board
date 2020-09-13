e31800f7d907ab7dfc64a386d200aa3a43fcff98
Vlad <wlandex@yandex.ru>
2020-09-14 01:37:57 +0300

    made some changes

--- main.js
+++ main.js
@@ -1,25 +1,70 @@
 const submit = document.querySelector("#btn");
-const inputTitle = document.querySelector("#inputTitle");
-const inputDesc = document.querySelector("#inputDescription");
-const card = document.getElementsByClassName("card");
-const cardTitle = document.getElementsByClassName("card__title");
-const cardDesc = document.getElementsByClassName("card__desc");
+const board = document.getElementById("board");
 let arr = [];
-const deleteBtn = document.querySelector("#deleteBtn");
-const editBtn = document.querySelector("#editBtn");
 
-submit.addEventListener("click", function () {
-  title = inputTitle.value;
-  desc = inputDesc.value;
-  card[0].style.display = "block";
+submit.addEventListener("click", () => {
+  const inputTitle = document.querySelector("#inputTitle");
+  const inputDesc = document.querySelector("#inputDescription");
+  board.style.display = "block";
 
-  arr.push(title, desc);
-  console.log(arr);
-  arr.forEach(function (item) {
-    cardTitle[0].innerHTML += `Title: ${item}`;
-    arr.splice(0, 1);
+  arr.push({
+    title: inputTitle.value,
+    desc: inputDesc.value,
   });
-  arr.forEach(function (item) {
-    cardDesc[0].innerHTML += `Description: ${item}`;
-  });
+  renderCards();
 });
+
+function renderCards() {
+  board.innerHTML = "";
+  for (let i = 0; i < arr.length; i++)
+    board.innerHTML += `<div class="card" id="cardNr${i}">
+  <div class="card__info">
+    <p>Title: ${arr[i].title}</p>
+    <p>Description: ${arr[i].desc}</p>    
+  </div>
+  <div class="buttons">
+    <button type="button" index="${i}" id="deleteBtn">Delete</button>
+    <button type="button" index="${i}" id="editBtn">Edit</button>
+  </div>
+</div>`;
+
+  addDeleteEvent();
+  addEditEvent();
+
+  console.log(arr);
+}
+
+function addDeleteEvent() {
+  document.querySelectorAll("#deleteBtn").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let index = event.currentTarget.getAttribute("index");
+      arr.splice(index, 1);
+      renderCards();
+    });
+  });
+}
+
+function addEditEvent() {
+  document.querySelectorAll("#editBtn").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let index = event.currentTarget.getAttribute("index");
+      const element = document.getElementById(`cardNr${index}`);
+      element.innerHTML = "";
+      element.innerHTML = `<form class="card__info">
+      <input id="editedTitle" type="text" value="${arr[index].title}"></input>
+      <input id="editedDesc" type="text" value="${arr[index].desc}"></input>
+      <input id="editedIndex" type="hidden" value="${index}"></input>
+      <button type="button" id="saveAfterEdit">Submit</button>
+      </form>`;
+
+      document.getElementById("saveAfterEdit").addEventListener("click", () => {
+        const inputTitle = document.querySelector("#editedTitle").value;
+        const inputDesc = document.querySelector("#editedDesc").value;
+        const editedIndex = document.querySelector("#editedIndex").value;
+        arr[editedIndex].title = inputTitle;
+        arr[editedIndex].desc = inputDesc;
+        renderCards();
+      });
+    });
+  });
+}
